e0b35065ca1461c995ecbc05c054a413b56048e8
sofiene chihi <chihisofiene2@gmail.com>
2023-11-23 20:55:50 +0100

    responsive design

--- frontend/src/app/components/home/bots-grid/bots-grid.component.ts
+++ frontend/src/app/components/home/bots-grid/bots-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConversationService } from '../../../services/conversation.service';
 import { Router } from '@angular/router';
 import { LocalstorageService } from '../../../services/localstorage.service';
@@ -8,17 +8,22 @@ import { LocalstorageService } from '../../../services/localstorage.service';
   templateUrl: './bots-grid.component.html',
   styleUrl: './bots-grid.component.css',
 })
-export class BotsGridComponent {
+export class BotsGridComponent implements OnInit {
+  isPhone: boolean = false;
+  gridColumns: number = 3
+  rowHeight1: number = 3
+  rowHeight2: number = 2
+
   items: any[] = [
     {
       id: 1,
-      name: 'Ecommerce Shop Assistant Chatbot',
+      name: 'Ecommerce Shop Chatbot',
       image: 'ecommerce-image.webp',
     },
     { id: 2, name: 'Customer Service Chatbot', image: 'customer-service.png' },
     {
       id: 3,
-      name: 'Restaurant Orderes Chatbot',
+      name: 'Pizzeria Chatbot',
       image: 'restaurant-order.png',
     },
   ];
@@ -26,8 +31,24 @@ export class BotsGridComponent {
   constructor(
     private conversationService: ConversationService,
     private router: Router,
-    private localStorageService: LocalstorageService
+    private localStorageService: LocalstorageService,
   ) {}
+
+  ngOnInit(): void {
+    this.checkIfPhone();
+    if (this.isPhone) {
+      this.gridColumns = 1
+      this.rowHeight1 = 1.7
+      this.rowHeight2 = 1
+    }
+  }
+
+  private checkIfPhone(): void {
+    const windowWidth = window.innerWidth
+
+    const phoneWidthThreshold = 768; 
+    this.isPhone = windowWidth < phoneWidthThreshold;
+  }
 
   openNewConversation() {
     this.conversationService.createConversation().subscribe(
